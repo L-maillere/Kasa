69550f3ae828777ed09fe9b97e1aad65c98d3055
Louis Maillere <louis.maillere@gmail.com>
2023-09-01 01:45:37 +0200

    léger fix + refonte architecture routage

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,12 +1,29 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from "./pages/About/About.jsx";
+import Error from "./pages/Error/Error.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import HousingDetails from "./pages/HousingDetails/HousingDetails.jsx";
 import "./styles/main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// Initialisation de l'application avec le routage
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<React.StrictMode>
+		<Router>
+			<Header />
+	  			<main>
+      				<Routes>
+        				<Route path="/" element={<Home />} />
+        				<Route path="/about" element={<About />} />
+        				<Route path="/housing/:housingId" element={<HousingDetails />} />
+        				<Route path="*" element={<Error />} />
+      				</Routes>
+	  			</main>
+      		<Footer />
+    	</Router>
+ 	 </React.StrictMode>
 );
