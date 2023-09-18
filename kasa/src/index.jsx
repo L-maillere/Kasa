e45d6d18de2f0ff900e5eb8af2518b617332eef1
Louis Maillere <louis.maillere@gmail.com>
2023-09-18 18:24:17 +0200

    fix import + fix gestion erreur id logement

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from "./pages/About/About.jsx";
-import Error from "./pages/Error/Error.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Header from "./components/Header/Header.jsx";
-import Home from "./pages/Home/Home.jsx";
+import Error from "./pages/Error/error.jsx";
+import Footer from "./components/Footer/footer.jsx";
+import Header from "./components/Header/header.jsx";
+import Home from "./pages/Home/home.jsx";
 import HousingDetails from "./pages/HousingDetails/HousingDetails.jsx";
 import "./styles/main.scss";
 
--- kasa/src/pages/HousingDetails/HousingDetails.jsx
+++ kasa/src/pages/HousingDetails/HousingDetails.jsx
@@ -2,12 +2,16 @@ import { useParams } from "react-router-dom"
 import { housingList } from "../../datas/housing";
 import Carousel from "../../components/Carousel/Carousel";
 import HousingDescription from "../../components/HousingDescription/HousingDescription";
+import Error from "../Error/error.jsx"
 
 
 function HousingDetails() {
     const { housingId } = useParams(); // Récupère l'ID du logement à partir de l'URL
     const housing = housingList.find(house => house.id === housingId); // Trouve le logement correspondant dans les données
 
+    if (!housing) {
+        return <Error />
+    }
     return (
         <div className="housing-wrapper">
             <Carousel slides={housing.pictures}/>
